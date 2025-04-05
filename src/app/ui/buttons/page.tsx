d5d52f4173729dd61573d723303d57048600dd25
Krish-Das <krishdasatgames@gmail.com>
2025-04-05 16:30:45 +0530

    feat: add color and variants prop to buttons

---
+++ src/app/ui/buttons/page.tsx
@@ -0,0 +1,103 @@
+"use client"
+
+import { VariantProps } from "class-variance-authority"
+
+import { Button, buttonVariants } from "@/components/ui/button"
+import { Container } from "@/components/ui/container"
+import { PagePadding } from "@/components/ui/page-padding"
+import { Section } from "@/components/ui/section"
+import { IonPlay } from "@/components/icons/ion-icons"
+import { Heading, InlineCode } from "@/components/typography"
+
+const variants: VariantProps<typeof buttonVariants>["variant"][] = [
+  "gray",
+  "filled",
+  "tinted",
+  "ghost",
+]
+const colors: VariantProps<typeof buttonVariants>["color"][] = [
+  "blue",
+  "red",
+  "yellow",
+  "green",
+  "mint",
+  "teal",
+  "cyan",
+  "indigo",
+  "purple",
+  "pink",
+  "brown",
+  "gray",
+]
+const sizes: VariantProps<typeof buttonVariants>["size"][] = ["lg", "md", "sm"]
+
+export default function Page() {
+  return (
+    <PagePadding>
+      <Container shorterWidth asChild>
+        <Section>
+          <Heading level="h3" className="mt-0">
+            Variants and Colors
+          </Heading>
+
+          {colors.map(color => (
+            <div key={color} className="mb-3 flex flex-wrap items-center gap-2">
+              {variants.map(variant => (
+                <Button
+                  key={`${color}-${variant}`}
+                  color={color}
+                  variant={variant}
+                >
+                  {`${color} ${variant}`}
+                </Button>
+              ))}
+            </div>
+          ))}
+        </Section>
+      </Container>
+
+      <Container shorterWidth asChild>
+        <Section>
+          <Heading level="h3" className="mt-0">
+            Sizes
+          </Heading>
+          {sizes.map(size => (
+            <div key={size} className="mb-3 flex flex-wrap items-center gap-2">
+              {variants.map(variant => (
+                <Button
+                  key={`${size}-${variant}`}
+                  size={size}
+                  variant={variant}
+                >
+                  {`${size} ${variant}`}
+                </Button>
+              ))}
+            </div>
+          ))}
+        </Section>
+      </Container>
+
+      <Container shorterWidth asChild>
+        <Section>
+          <Heading level="h3" className="mt-0">
+            <InlineCode>{"isIconOnly"}</InlineCode> prop:
+          </Heading>
+          {sizes.map(size => (
+            <div key={size} className="mb-3 flex flex-wrap items-center gap-2">
+              {variants.map(variant => (
+                <Button
+                  key={`${size}-${variant}`}
+                  size={size}
+                  variant={variant}
+                  isIconOnly
+                >
+                  <IonPlay />
+                </Button>
+              ))}
+            </div>
+          ))}
+        </Section>
+      </Container>
+    </PagePadding>
+  )
+}
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -12,12 +12,35 @@ import { cn } from "@/lib/utils"
 const buttonVariants = cva(
   [
     "inline-flex h-fit w-fit items-center justify-center font-medium whitespace-nowrap",
-    "focus:outline-none focus-visible:outline-none",
+    "ring-offset-background/0 ring-offset-2 focus:outline-none focus-visible:ring-2 focus-visible:outline-none",
     "touch-none cursor-default select-none", // cursor and select
-    "disabled:pointer-events-none disabled:opacity-50", // Disabled
+    "disabled:pointer-events-none disabled:opacity-50 disabled:text-label-tertiary disabled:bg-fill-secondary", // Disabled
   ],
   {
     variants: {
+      variant: {
+        gray: "bg-fill-tertiary text-[var(--button-color)]",
+        filled: "bg-[var(--button-color)] text-[var(--button-fg,_white)]",
+        tinted:
+          "bg-[var(--button-color)]/[var(--fill-tertiary-opacity)] text-[var(--button-color)]",
+        ghost:
+          "text-[var(--button-color)] hover:bg-[var(--button-color)]/[var(--fill-quaternary-opacity)]",
+      },
+      color: {
+        blue: "[--button-color:var(--ios-blue)]",
+        red: "[--button-color:var(--ios-red)]",
+        orange: "[--button-color:var(--ios-orange)]",
+        yellow: "[--button-color:var(--ios-yellow)]",
+        green: "[--button-color:var(--ios-green)]",
+        mint: "[--button-color:var(--ios-mint)]",
+        teal: "[--button-color:var(--ios-teal)]",
+        cyan: "[--button-color:var(--ios-cyan)]",
+        indigo: "[--button-color:var(--ios-indigo)]",
+        purple: "[--button-color:var(--ios-purple)]",
+        pink: "[--button-color:var(--ios-pink)]",
+        brown: "[--button-color:var(--ios-brown)]",
+        gray: "[--button-color:var(--gray-1)]",
+      },
       size: {
         lg: "gap-1 rounded-xl px-5 py-3.5 [&_svg]:text-lg leading-5.5",
         md: "gap-1 rounded-full px-3.5 py-1.75 text-sm [&_svg]:text-base leading-5",
@@ -46,6 +69,8 @@ const buttonVariants = cva(
       },
     ],
     defaultVariants: {
+      variant: "gray",
+      color: "blue",
       size: "md",
       isIconOnly: false,
     },
@@ -55,11 +80,13 @@ const buttonVariants = cva(
 type ButtonProps = RacButtonProps & VariantProps<typeof buttonVariants>
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, size, isIconOnly, ...rest }, ref) => {
+  ({ className, variant, color, size, isIconOnly, ...rest }, ref) => {
     return (
       <RacButton
         ref={ref}
-        className={cn(buttonVariants({ size, isIconOnly, className }))}
+        className={cn(
+          buttonVariants({ variant, color, size, isIconOnly, className })
+        )}
         {...rest}
       />
     )
