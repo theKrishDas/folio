12e809d2c75a044493339e2a3b910d36cbb8f778
Krish-Das <krishdasatgames@gmail.com>
2025-04-05 15:00:37 +0530

    feat: add placeholder content on the lander

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,5 +1,16 @@
-import Welcome from "@/components/next-kit-welcome"
+import { PagePadding } from "@/components/ui/page-padding"
+import About from "@/components/lander/About"
+import CTA from "@/components/lander/CTA"
+import Location from "@/components/lander/Location"
 
 export default function Home() {
-  return <Welcome />
+  return (
+    <PagePadding>
+      <About />
+
+      <Location />
+
+      <CTA />
+    </PagePadding>
+  )
 }
