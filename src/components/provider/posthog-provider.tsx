8b1e5207d7e684a59e0d587cde802b41ec1c7fd6
Krish-Das <krishdasatgames@gmail.com>
2025-04-27 11:56:53 +0530

    feat: configure Next.js rewrites as PostHog proxy
    - Add rewrite rules in `next.config.js`
    - Change api_host to use local "/ingest" proxy
    - Add separate ui_host for PostHog UI

--- src/components/provider/posthog-provider.tsx
+++ src/components/provider/posthog-provider.tsx
@@ -6,8 +6,12 @@ import { PostHogProvider as PHProvider, usePostHog } from "posthog-js/react"
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host:
-        process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+      // api_host:
+      //   process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+
+      // Reverse-proxy to get around add-blockers
+      api_host: "/ingest",
+      ui_host: "https://us.posthog.com",
       person_profiles: "identified_only", // Or 'always' to create profiles for anonymous users as well
       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
     })
