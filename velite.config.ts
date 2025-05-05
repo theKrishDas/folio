83dfb1eb5d107d45efe38417db3eeb5ccebdc311
Krish-Das <krishdasatgames@gmail.com>
2025-05-05 21:18:23 +0530

    feat: add velite config

---
+++ velite.config.ts
@@ -0,0 +1,35 @@
+import { defineCollection, defineConfig, s } from "velite"
+
+// WARN: I don't like this here
+export const baseWritingsURL = "w" as const
+
+const computedFields = <T extends { slug: string }>(data: T) => ({
+  ...data,
+  slugAsParams: [baseWritingsURL, data.slug.split("/").slice(1).join("/")].join(
+    "/"
+  ),
+})
+
+// NOTE: Visit the docs
+// https://velite.js.org/guide/define-collections#collection-schema-options
+const writings = defineCollection({
+  name: "Writings",
+  pattern: "writings/**/*.mdx",
+  schema: s
+    .object({
+      title: s.string().max(99),
+      slug: s.path(),
+      summary: s.string(),
+      date: s.isodate(),
+      published: s.boolean().default(true),
+      cover: s.image().optional(),
+      body: s.mdx(),
+    })
+    .transform(computedFields),
+})
+
+export default defineConfig({
+  collections: { writings },
+  strict: true,
+  output: { clean: true }, // Clean output directories before build.
+})
