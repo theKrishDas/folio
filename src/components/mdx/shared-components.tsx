73c8f6acea28212aa840b6d41435085812ae69c0
Krish-Das <krishdasatgames@gmail.com>
2025-05-05 21:49:43 +0530

    feat: add mdx-components

---
+++ src/components/mdx/shared-components.tsx
@@ -0,0 +1,34 @@
+import {
+  Anchor,
+  Blockquote as blockquote,
+  Cite,
+  // Code, // TODO: add this component
+  Divider,
+  Em as em,
+  Image as img,
+  InlineCode,
+  Li as li,
+  Paragraph as p,
+  Strong as strong,
+  Ul as ul,
+} from "../typography"
+import { Heading, HeadingProps } from "../typography/heading"
+
+const SharedComponents = {
+  h2: (props: HeadingProps) => <Heading level="h2" {...props} />,
+  h3: (props: HeadingProps) => <Heading level="h3" {...props} />,
+  h4: (props: HeadingProps) => <Heading level="h4" {...props} />,
+  p,
+  strong,
+  em,
+  ul,
+  li,
+  blockquote,
+  Cite,
+  a: Anchor,
+  code: InlineCode,
+  // pre: Code,
+  hr: Divider,
+  img,
+}
+export default SharedComponents
