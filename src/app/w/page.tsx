f8d8a7be5707151bb1e838b61111a4b05dfcb1d6
Krish-Das <krishdasatgames@gmail.com>
2025-05-13 19:44:57 +0530

    feat: redesign the index page for writings

--- src/app/w/page.tsx
+++ src/app/w/page.tsx
@@ -1,17 +1,17 @@
-import Image from "next/image"
 import { writings } from "#/writings"
 
-import { formatDate } from "@/lib/utils"
+import { cn, formatDate } from "@/lib/utils"
 import Back from "@/components/ui/back-button"
+import { Button } from "@/components/ui/button"
 import { Container } from "@/components/ui/container"
-import { List } from "@/components/ui/item-list"
 import { PagePadding } from "@/components/ui/page-padding"
 import { Section, SectionHeading } from "@/components/ui/section"
+import { Heading } from "@/components/typography"
 
 export default function Page() {
   return (
     <PagePadding>
-      <Container asChild>
+      <Container asChild shorterWidth>
         <Section asChild className="relative">
           <nav>
             <Back url="/" />
@@ -19,37 +19,81 @@ export default function Page() {
         </Section>
       </Container>
 
-      <Container asChild>
+      <Container shorterWidth asChild>
         <Section className="space-y-6">
-          <SectionHeading
-            styleAs="h3"
-            className="sr-only mt-0 mb-4"
-            id="about-heading"
-          >
-            All Writings of Upsher
-            <span aria-hidden className="touch-none select-none">
-              {"_"}
-            </span>
-          </SectionHeading>
+          <Heading className="max-w-[16ch]">
+            Unacceptable behaviour of writings.
+          </Heading>
 
-          <div className="relative aspect-3/1 w-full overflow-hidden rounded-lg">
-            <Image
-              src="/images/writings-banner.png"
-              alt="liquid gradient banner"
-              style={{ objectFit: "cover" }}
-              fill
-            />
+          <div className="h-2 w-10" aria-hidden />
+
+          {/* Categories */}
+          <div className="w-full overflow-x-auto">
+            <ul className="flex w-fit flex-nowrap gap-2.5">
+              {["All Categories", "Documentation", "Guide", "Personal"].map(
+                (item, idx) => (
+                  <li
+                    className={cn(
+                      "bg-fill-quaternary text-label-secondary rounded-lg px-2 py-1.5 text-xs leading-none tracking-wide whitespace-nowrap select-none md:inline-block",
+                      idx === 0 && "bg-ios-blue text-white"
+                    )}
+                    key={idx}
+                  >
+                    <label className="inline-flex">{item}</label>
+                  </li>
+                )
+              )}
+            </ul>
           </div>
 
-          {/* TODO: Handle the no-writings state */}
-          <List.Root noGap>
-            {writings.map(({ slugAsParams, title, date }) => (
-              <List.Item key={slugAsParams} href={`/w/${slugAsParams}`}>
-                <List.Title>{title}</List.Title>
-                <List.Subtitle>{formatDate(date)}</List.Subtitle>
-              </List.Item>
+          {/* Lists */}
+          <ul className="flex touch-none flex-col gap-1.5 select-none">
+            {writings.map(w => (
+              <li
+                key={w.slug}
+                className="hover:bg-fill-secondary flex flex-col gap-2 rounded-xl px-0 py-2.5 transition-all duration-250 hover:px-3"
+              >
+                <div className="align-start flex gap-4">
+                  <h2 className="flex-1 truncate text-[0.9rem] leading-none font-medium">
+                    {w.title}
+                  </h2>
+                  <p className="text-label-secondary font-jetbrains-mono text-xs leading-none">
+                    {formatDate(w.date).split(",")[0]}
+                  </p>
+                </div>
+
+                <div className="max-w-[calc(100%-6rem)]">
+                  <p className="text-label-secondary truncate text-xs font-medium">
+                    {w.summary}
+                  </p>
+                </div>
+              </li>
             ))}
-          </List.Root>
+          </ul>
+
+          <div className="h-2 w-10" aria-hidden />
+
+          {/* CTA: Subscribe to the writings */}
+          <div>
+            <SectionHeading className="mt-0 pl-1">
+              Subscribe to new writings
+            </SectionHeading>
+
+            <div className="inline-flex w-full gap-2">
+              <div
+                className={cn(
+                  "bg-fill-tertiary text-label-secondary w-full max-w-70 rounded-lg text-sm select-none",
+                  "px-3.5 py-1.75 leading-5",
+                  "rounded-xl px-5 py-3.5 leading-5.5"
+                )}
+              >
+                Email adress...
+              </div>
+              <Button size="lg" isDisabled>
+                Subscribe
+              </Button>
+            </div>
+          </div>
         </Section>
       </Container>
     </PagePadding>
